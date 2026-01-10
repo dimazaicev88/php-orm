dbf2261154fde56a01e99da9f52d3beefa1e43ef
dimaz <dimazaicev88@mail.com>
2026-01-10 16:28:24 +0300

    fix changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lynx/lynx',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '92135dc5641879ff4ed2efa8e584cd9a279bec94',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lynx/lynx' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '92135dc5641879ff4ed2efa8e584cd9a279bec94',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
